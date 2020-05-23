13118b7e59d5b8e0205595863dadc602b11bc8be
couch-potahto <39213650+couch-potahto@users.noreply.github.com>
2020-05-23 09:18:48 +0800

    migrate Employee model

--- HRapp/models.py
+++ HRapp/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.core.validators import
 
 # Create your models here.
 class Employee(models.Model):
-	employee_id = models.CharField(max_length=200, required=True)
-	login = models.CharField(max_length=200, required=True)
-	name = models.CharField(max_length=200, required=True)
+	employee_id = models.CharField(max_length=200, unique=True)
+	login = models.CharField(max_length=200, unique=True)
+	name = models.CharField(max_length=200)
 	salary = models.DecimalField(max_digits=10, decimal_places=2)
